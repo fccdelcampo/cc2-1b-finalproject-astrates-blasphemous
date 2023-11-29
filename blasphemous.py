395090509cd3320f9ca688ffe8eda7b2d6ff783e
fccdelcampo <145944562+fccdelcampo@users.noreply.github.com>
2023-11-29 08:42:10 +0800

    Update blasphemous.py

--- blasphemous.py
+++ blasphemous.py
@@ -1,4 +1,5 @@
 import random
+import time
 
 class Cell:
     def __init__(self, symbol, passable=True):
@@ -9,26 +10,48 @@ class Grid:
     def __init__(self, width, height):
         self.width = width
         self.height = height
-        self.cells = [[Cell('.') for _ in range(width)] for _ in range(height)]
+        self.cells = [[Cell('#') for _ in range(width)] for _ in range(height)]
 
-    def spawn_entity(self, entity_symbol):
-        x, y = random.randint(0, self.width - 1), random.randint(0, self.height - 1)
-        while self.cells[y][x].symbol != '.':
-            x, y = random.randint(0, self.width - 1), random.randint(0, self.height - 1)
+    def generate_maze(self, start_x, start_y):
+        stack = [(start_x, start_y)]
+
+        while stack:
+            current_x, current_y = stack[-1]
+
+            # Get neighbors
+            neighbors = [(current_x + 2, current_y), (current_x - 2, current_y),
+                         (current_x, current_y + 2), (current_x, current_y - 2)]
+            neighbors = [(x, y) for x, y in neighbors if 0 <= x < self.width and 0 <= y < self.height]
+
+            unvisited_neighbors = [(x, y) for x, y in neighbors if self.cells[y][x].symbol == '#']
+
+            if unvisited_neighbors:
+                next_x, next_y = random.choice(unvisited_neighbors)
+                wall_x, wall_y = (current_x + next_x) // 2, (current_y + next_y) // 2
+
+                self.cells[wall_y][wall_x] = Cell(' ')
+                self.cells[next_y][next_x] = Cell(' ')
+                stack.append((next_x, next_y))
+            else:
+                stack.pop()
+
+    def spawn_entity(self, entity_symbol, x, y):
         self.cells[y][x] = Cell(entity_symbol)
         return x, y
 
     def remove_entity(self, x, y):
-        self.cells[y][x] = Cell('.')
+        self.cells[y][x] = Cell(' ')
 
-    def print_grid(self, player_x, player_y):
+    def move_entity(self, old_x, old_y, new_x, new_y):
+        if self.cells[new_y][new_x].passable:
+            self.cells[new_y][new_x], self.cells[old_y][old_x] = self.cells[old_y][old_x], Cell(' ')
+
+    def print_grid(self):
         for y in range(self.height):
             for x in range(self.width):
-                if x == player_x and y == player_y:
-                    print('P', end=' ')
-                else:
-                    print(self.cells[y][x].symbol, end=' ')
+                print(self.cells[y][x].symbol, end=' ')
             print()
+
 
 class Entity:
     def __init__(self, name, health, speed, attack):
@@ -45,73 +68,80 @@ class Entity:
     def is_alive(self):
         return self.health > 0
 
+def fade_in_text(text, delay=0.001):
+    for char in text:
+        print(char, end='', flush=True)
+        time.sleep(delay)
+    print()
+
 def main():
-    
     name = input("What's your name?\n")
     welcome = f"""BACKGROUND: Welcome to Blasphemous, {name}. Play as the Penitent One - a sole survivor 
-    of the massacre of the ‘Silent Sorrow’. Trapped in an endless cycle of death and 
-    rebirth, it’s down to him to free the world from this terrible fate and reach the 
-    origin of his anguish."""
+     of the massacre of the ‘Silent Sorrow’. Trapped in an endless cycle of death and 
+     rebirth, it’s down to him to free the world from this terrible fate and reach the 
+     origin of his anguish."""
 
     story = """STORY: The story of Blasphemous is set in the deeply religious realm of Cvstodia, where the 
-    Sorrowful Miracle manifests itself to both the pious and sinful in cruel and unusual 
-    ways. Every pain of the soul is tangible within everyone and everywhere, whether in 
-    the form of a blessing or a punishment. With such a divine and unfathomable will at 
-    large, all one can do is make penance.
+     Sorrowful Miracle manifests itself to both the pious and sinful in cruel and unusual 
+     ways. Every pain of the soul is tangible within everyone and everywhere, whether in 
+     the form of a blessing or a punishment. With such a divine and unfathomable will at 
+     large, all one can do is make penance.
 
-    A warrior forever vowed to silence, The Penitent One, awakens in a sanctuary 
-    surrounded by his slain comrades. Under the guidance of Deogracias and with the 
-    strength of the sword of guilt, he fights his way to the Cradle of Affliction to 
-    fulfill this ultimate penance. Throughout his journey, he traverses a land ravaged 
-    by the Grievous Miracle, battling beasts consumed by faith and rage, communing with 
-    holy figures, assisting others at the mercy of The Miracle, and freeing tormented 
-    souls from eternal punishment.
+     A warrior forever vowed to silence, The Penitent One, awakens in a sanctuary 
+     surrounded by his slain comrades. Under the guidance of Deogracias and with the 
+     strength of the sword of guilt, he fights his way to the Cradle of Affliction to 
+     fulfill this ultimate penance. Throughout his journey, he traverses a land ravaged 
+     by the Grievous Miracle, battling beasts consumed by faith and rage, communing with 
+     holy figures, assisting others at the mercy of The Miracle, and freeing tormented 
+     souls from eternal punishment.
 
-    The Penitent One's guilt is his strength; his pilgrimage is his penance. But 
-    in the end, it is the will of The Miracle to decide if his guilt is enough to 
-    earn him salvation.
+     The Penitent One's guilt is his strength; his pilgrimage is his penance. But 
+     in the end, it is the will of The Miracle to decide if his guilt is enough to 
+     earn him salvation.
 
-    Blasphemous begins in the Brotherhood of the Silent Sorrow, a religious order opposed to
-    His Holiness Escribar's authority, after all its members have been massacred. The last 
-    of their kind, the Penitent One, is resurrected by the Miracle and departs on a pilgrimage."""
+     Blasphemous begins in the Brotherhood of the Silent Sorrow, a religious order opposed to
+     His Holiness Escribar's authority, after all its members have been massacred. The last 
+     of their kind, the Penitent One, is resurrected by the Miracle and departs on a pilgrimage."""
 
     menu = """
-    To play, enter the number of your choosing below.
-    1 - START GAME
-    2 - SEE INITIAL STATS
-    3 - QUIT (Are you sure that you want to quit?)
-    """
+     To play, enter the number of your choosing below.
+     1 - START GAME
+     2 - SEE INITIAL STATS
+     3 - QUIT (Are you sure that you want to quit?)
+     """
 
     border = "=========================================================================================="
-    
+
     credits = """
-    Thank you for playing our game. Whether you won or lost doesn't matter, as long as you enjoyed.
-    Hope you play our game again.
-    
-    BLASPHEMOUS CREATED BY:
-        Francis Carlo Del Campo
-        Miguel Liceralde III
-        Mark Dunstan Gabor
-        Daniel Javillonar
-    """
+     Thank you for playing our game. Whether you won or lost doesn't matter, as long as you enjoyed.
+     Hope you play our game again.
+
+     BLASPHEMOUS CREATED BY:
+         Francis Carlo Del Campo
+         Miguel Liceralde III
+         Mark Dunstan Gabor
+         Daniel Javillonar
+     """
 
     print(border)
-    print(welcome)
+    fade_in_text(welcome)
     print(border)
-    print(story)
+    fade_in_text(story)
     print(border)
-    
+
     width = 20
     height = 10
     num_enemies = 3
 
     grid = Grid(width, height)
-    player_x, player_y = grid.spawn_entity('P')
+    grid.generate_maze(1, 1)  # Start generating maze from (1, 1) to ensure an open space
+    player_x, player_y = grid.spawn_entity('P', 1, 1)
 
     player = Entity("Hero", 100, 10, 20)
-    enemies = [Entity(f"Enemy{i}", random.randint(20, 50), random.randint(5, 10), random.randint(5, 15)) for i in range(num_enemies)]
-    enemy_positions = [grid.spawn_entity('E') for _ in range(num_enemies)]
-    
+    enemies = [Entity(f"Enemy{i}", random.randint(20, 50), random.randint(5, 10), random.randint(5, 15)) for i in
+               range(num_enemies)]
+    enemy_positions = [grid.spawn_entity('E', random.randint(0, width - 1), random.randint(0, height - 1)) for _ in range(num_enemies)]
+
     while True:
         navchoice = input(menu)
         print(border)
@@ -121,7 +151,7 @@ def main():
             print("Welcome to the RPG Game!")
 
             while player.is_alive():
-                grid.print_grid(player_x, player_y)
+                grid.print_grid()
                 print("\nPlayer Stats:")
                 print(f"Name: {player.name}")
                 print(f"HP: {player.health}")
@@ -145,13 +175,14 @@ def main():
                 elif action == 'd' and player_x < width - 1 and grid.cells[player_y][player_x + 1].passable:
                     new_player_x += 1
 
+
                 # Check for enemy encounters and attacks
                 for enemy, (enemy_x, enemy_y) in zip(enemies, enemy_positions):
                     if (abs(new_player_x - enemy_x) == 1 and new_player_y == enemy_y) or \
-                    (new_player_x == enemy_x and abs(new_player_y - enemy_y) == 1):
+                            (new_player_x == enemy_x and abs(new_player_y - enemy_y) == 1):
                         print(f"Encountered {enemy.name}!")
                         while player.is_alive() and enemy.is_alive():
-                            grid.print_grid(new_player_x, new_player_y)
+                            grid.print_grid()
                             print("\nPlayer Stats:")
                             print(f"Name: {player.name}")
                             print(f"HP: {player.health}")
@@ -185,7 +216,9 @@ def main():
                                 break
 
                 if player.is_alive():
+                    grid.move_entity(player_x, player_y, new_player_x, new_player_y)
                     player_x, player_y = new_player_x, new_player_y
+
         elif navchoice == '2':
             print("These are your stats.")
             print("\nPlayer Stats:")
@@ -197,7 +230,6 @@ def main():
             print(credits)
             quit()
             break
-
 
 if __name__ == "__main__":
     main()
